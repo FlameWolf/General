77d3d9a08d776a2d339ff363e30db5110744a951
FlameWolf <agnijith@gmail.com>
2024-12-12 14:33:58 +0530

    Create enc-dec.js

---
+++ enc-dec.js
@@ -0,0 +1,27 @@
+const initVector = crypto.getRandomValues(new Uint8Array(64)); /* required for decryption */
+const [encoder, decoder] = [new TextEncoder(), new TextDecoder()];
+const encryptionKey = await crypto.subtle.generateKey(
+	{
+		name: "AES-GCM",
+		length: 256
+	},
+	true,
+	["encrypt", "decrypt"]
+);
+const getEncryptionParams = (key, data) => [
+	{
+		name: "AES-GCM",
+		iv: initVector
+	},
+	key,
+	data
+];
+const encryptMessage = async (key, plainText) => await crypto.subtle.encrypt(...getEncryptionParams(key, encoder.encode(plainText)));
+const decryptMessage = async (key, ciphertext) => decoder.decode(await crypto.subtle.decrypt(...getEncryptionParams(key, ciphertext)));
+/* Test implementation */
+const textToEncrypt = "മാസങ്ങളിൽ മേടം പ്രധാനം";
+const encryptedText = await encryptMessage(encryptionKey, textToEncrypt);
+const bufferAsString = new Uint8Array(encryptedText).reduce((data, byte) => data + String.fromCodePoint(byte), "");
+const stringAsBuffer = bufferAsString.split("").map(x => x.codePointAt(0));
+const decryptedText = await decryptMessage(encryptionKey, Uint8Array.from(stringAsBuffer));
+console.log(textToEncrypt === decryptedText ? ":)" : ":(");
