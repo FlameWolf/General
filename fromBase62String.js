a2ba75d69fb10606caf27ea8b5379528597c84b0
FlameWolf <agnijith@gmail.com>
2024-12-13 14:54:09 +0530

    Update fromBase62String.js

--- fromBase62String.js
+++ fromBase62String.js
@@ -1,9 +1,9 @@
-const fromBase62String = function (base62String) {
+const fromBase62String = function (value) {
 	const base = 62;
 	const digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const isNegative = base62String[0] === "-";
+	const isNegative = value[0] === "-";
 	let result = 0;
-	for (const char of base62String.slice(isNegative ? 1 : 0)) {
+	for (const char of value.slice(isNegative ? 1 : 0)) {
 		result = result * base + digits.indexOf(char);
 	}
 	return isNegative ? -result : result;
